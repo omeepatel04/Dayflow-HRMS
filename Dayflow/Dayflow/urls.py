ed43e994acfb19964bf55ffe9efba3a2aa6642c2
Nakshi Shah <nakshishah3108@gmail.com>
2026-01-03 11:25:54 +0530

    urls added

--- Dayflow/Dayflow/urls.py
+++ Dayflow/Dayflow/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('attendance/', include('attendance.urls')),
+    path('leaves/', include('leaves.urls')),
+    path('payroll/', include('payroll.urls')),
+    path('users/', include('users.urls')),
 ]
