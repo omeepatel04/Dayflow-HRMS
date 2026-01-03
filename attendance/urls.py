bfce6aa83fedd2dc8803ed3e94bbd46d97018faa
Nakshi Shah <nakshishah3108@gmail.com>
2026-01-03 11:20:16 +0530

    basic setup

--- attendance/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CheckInView, CheckOutView, MyAttendanceView, AllAttendanceView
-
-urlpatterns = [
-    path('check-in/', CheckInView.as_view(), name='check-in'),
-    path('check-out/', CheckOutView.as_view(), name='check-out'),
-    path('my-attendance/', MyAttendanceView.as_view(), name='my-attendance'),
-    path('all/', AllAttendanceView.as_view(), name='all-attendance'),
-]
